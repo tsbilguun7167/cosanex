bc5e50e9b38053f60e2944e8f468e3a2a0e04bd5
Ц. Билгүүн <tsbilguun7167@gmail.com>
2023-08-24 16:39:26 +0800

    seo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(private meta: Meta,private title: Title) {
+    this.meta.addTags([
+      {name: 'description', content: 'Cosanex - Legal Services'},
+      {name: 'author', content: 'Blueprint Solutions LLC'},
+      {name: 'keywords', content: 'cosanex, legal, legal-services, law, law-firm'}
+    ]);
+    this.setTitle('Home');
+  }
+  public setTitle( newTitle: string) {
+    this.title.setTitle( newTitle );
+    }
 }

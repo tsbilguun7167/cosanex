4d0c656cf74de99e097e411895eb122dd9ff6707
Ц. Билгүүн <tsbilguun7167@gmail.com>
2023-08-30 16:05:49 +0800

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,18 +4,8 @@ import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private meta: Meta,private title: Title) {
-    this.meta.addTags([
-      {name: 'description', content: 'Cosanex - Legal Services'},
-      {name: 'author', content: 'Blueprint Solutions LLC'},
-      {name: 'keywords', content: 'cosanex, legal, legal-services, law, law-firm'},
-    ]);
-    this.setTitle('Home');
-  }
-  public setTitle( newTitle: string) {
-    this.title.setTitle( newTitle );
-    }
+  constructor() {}
 }
